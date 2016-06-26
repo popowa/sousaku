596298d5c39752e338e83becc5622ce0cf23e841
Aya Komuro <komuro@serverworks.co.jp>
2016-06-26 17:28:22 +0900

    popupを見えやすくした

--- pixiv_custom_ui/options.js
+++ pixiv_custom_ui/options.js
@@ -7,10 +7,12 @@ $("#save").click(function () {
 });
 $("#clear").click(function(){
     chrome.storage.sync.clear();
+    $('.options').find('input:text').val('');
+    $('.options').find('input:checkbox').prop("checked",false);
     $('#status').append('オプションをクリアしました。ページのリロードをして下さい。');
     setTimeout(function(){
          $('#status').text("").faceOut("slow");
-    },800);
+    },1200);
 
 });
 
@@ -30,9 +32,10 @@ function save_options() {
       uiScoreOff: uiScoreOff
     }, function() {
         $('#status').append('保存しました');
+        restore_options();
         setTimeout(function(){
              $('#status').text("");
-        },800);
+        },1200);
     });
 };
 
@@ -47,27 +50,34 @@ function restore_options(){
     }, function(items) {
         $('#uiNotifyOn').prop("checked", items.uiNotifyOn);
         $('#uiNotifyOnNum').val(items.uiNotifyOnNum);
-            if(items.uiNotifyOn == true){
-                $('.popboard>a').addClass('unread');
-                $('span.count').html(items.uiNotifyOnNum);
-            };
+        if(items.uiNotifyOn == true){
+            $('.popboard>a').addClass('unread');
+            $('span.count').html(items.uiNotifyOnNum);
+        };
         $('#uiNotifyOff').prop("checked", items.uiNotifyOff);
-            if(items.uiNotifyOff == true){
-                $('.notifications').remove();
-            };
+        if(items.uiNotifyOff == true){
+            $('.notifications').remove();
+        };
         $('#uiUserIdHidden').val(items.uiUserIdHidden);
-            var userIds = null;
-            $("a.user").each(function(){
-                userIds = $(this).attr('data-user_id');
-                if(userIds == items.uiUserIdHidden){
-                    $(this).parent().remove();
-                };
+        if(items.uiUserIdHidden !== null){
+            var uiUserIdArray = items.uiUserIdHidden.split(',');
+            $.each(uiUserIdArray, function(index, eachUserId){
+                var userIds = null;
+                $("a.user").each(function(){
+                    userIds = $(this).attr('data-user_id');
+                    eachUserId = $.trim(eachUserId);
+                    if(userIds == eachUserId){
+                        $(this).parent().remove();
+                    };
+                });
             });
+        };
         $('#uiBKMOff').prop("checked", items.uiBKMOff);
+        if(items.uiBKMOff == true){
             $(".bookmark-count").each(function(){
-                $(this).html(0);
+                $(this).html("♥");
             });
-
+        };
         $('#uiScoreOff').prop("checked", items.uiScoreOff);
             if(items.uiScoreOff == true){
                 $('.score').remove();
